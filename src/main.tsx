0a26ce83df3f425883cb27f0f5dfeae9b6b6a5c4
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-02 13:25:40 -0400

    simplify main.tsx to use Devvit.addTrigger on PostSubmit event

--- src/main.tsx
+++ src/main.tsx
@@ -5,52 +5,10 @@ Devvit.configure({
   redditAPI: true,
 });
 
-// Add a menu item to the subreddit menu for instantiating the new experience post
-Devvit.addMenuItem({
-  label: 'Add my post',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async (_event, context) => {
-    const { reddit, ui } = context;
-    const subreddit = await reddit.getCurrentSubreddit();
-    await reddit.submitPost({
-      title: 'My devvit post',
-      subredditName: subreddit.name,
-      // The preview appears while the post loads
-      preview: (
-        <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">Loading ...</text>
-        </vstack>
-      ),
-    });
-    ui.showToast({ text: 'Created post!' });
-  },
-});
-
-// Add a post type definition
-Devvit.addCustomPostType({
-  name: 'Experience Post',
-  height: 'regular',
-  render: (context) => {
-    const { useState } = context;
-    const [counter, setCounter] = useState(0);
-
-    return (
-      <vstack height="100%" width="100%" gap="medium" alignment="center middle">
-        <image
-          url="logo.png"
-          description="logo"
-          imageHeight={256}
-          imageWidth={256}
-          height="48px"
-          width="48px"
-        />
-        <text size="large">{`Click counter: ${counter}`}</text>
-        <button appearance="primary" onPress={() => setCounter((counter) => counter + 1)}>
-          Click me!
-        </button>
-      </vstack>
-    );
+Devvit.addTrigger({
+  event: 'PostSubmit', // Event name from above
+  onEvent: async (event) => {
+    console.log(`Received OnPostSubmit event:\n${JSON.stringify(event)}`);
   },
 });
 
