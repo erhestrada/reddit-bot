f177ff93c5859cd7f7732d6923c7f97067fc5072
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-10 19:28:48 -0400

    initialize isTwitchClip, isYoutubeClip, isKickClip

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,29 @@ Devvit.configure({
   redditAPI: true,
 });
 
+function isTwitchClip(url: string) {
+  if (url.includes("twitch.tv") || url.includes("clips.twitch.tv")) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isYoutubeClip(url: string) {
+
+}
+
+function isKickClip(url: string) {
+
+}
+
 Devvit.addTrigger({
   event: 'PostSubmit', // Event name from above
   onEvent: async (event) => {
-    console.log(`Received OnPostSubmit event:\n${JSON.stringify(event)}`);
+    //console.log(`Received OnPostSubmit event:\n${JSON.stringify(event)}`);
+    const eventData = JSON.parse(JSON.stringify(event));
+    const url = eventData.post.url;
+    console.log(`Post URL: ${url}`);
   },
 });
 
